431e9b65ef8176218141ac7a6c26353e6ff2f29a
KS <kitsidhu@gmail.com>
2021-07-14 20:55:14 -0400

    WIP pushing up a working version, commenting out broken code

--- client-side/js/components/Card.js
+++ client-side/js/components/Card.js
@@ -10,22 +10,22 @@
 //     `;
 // }
 
-export default function Card(cards) {
-  let next = 0;
-      ${cards.collection.items
-        .map((card) => {
-          const cardTitle = card.data[0].title;
-          const cardImage = card.links[0].href;
-          const cardShortDescription = card.data[0].description_508;
-          const cardLongDescription = card.data[0].description;
-          return `
-          <div class='card__container'>
-            <input type='hidden' id='cardId' value=${card.data[0].nasa_id} />
-            <h2 class='card__title'>${cardTitle}</h2>
-            <div class='card__img'><img src=${cardImage} alt='' /></div>
-            <p class='card__desc'>${cardLongDescription}</p>
-            </div>
-            `;
-        })
-        .join('')}
-  }
+// export default function Card(cards) {
+//   let next = 0;
+//       ${cards.collection.items
+//         .map((card) => {
+//           const cardTitle = card.data[0].title;
+//           const cardImage = card.links[0].href;
+//           const cardShortDescription = card.data[0].description_508;
+//           const cardLongDescription = card.data[0].description;
+//           return `
+//           <div class='card__container'>
+//             <input type='hidden' id='cardId' value=${card.data[0].nasa_id} />
+//             <h2 class='card__title'>${cardTitle}</h2>
+//             <div class='card__img'><img src=${cardImage} alt='' /></div>
+//             <p class='card__desc'>${cardLongDescription}</p>
+//             </div>
+//             `;
+//         })
+//         .join('')}
+//   }
