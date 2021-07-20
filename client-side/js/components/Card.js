62136bda22ed6a2075cdabed57ca2e5b131549b7
KS <kitsidhu@gmail.com>
2021-07-20 10:53:25 -0400

    display single card on click of title in list

--- client-side/js/components/Card.js
+++ client-side/js/components/Card.js
@@ -1,7 +1,7 @@
 export default function Card(card) {
     return `
     <div class='card__container'>
-        <input type='hidden' id='cardId' value=${card.data[0].nasa_id} />
+        <input type='hidden' id='cardId${card.data[0].nasa_id}' value=${card.data[0].nasa_id} />
         <h2 class='card__title'>${card.data[0].title}</h2>
         <h3>${card.data[0].nasa_id}</h3>
         <div class='card__img'><img src=${card.links[0].href} alt='' /></div>
