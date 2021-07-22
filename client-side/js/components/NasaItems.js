36209f11e7ba0edb0e6bd60ebb7fdb2e50217563
KS <kitsidhu@gmail.com>
2021-07-22 18:17:19 -0400

    filter for nasa items that contains images

--- client-side/js/components/NasaItems.js
+++ client-side/js/components/NasaItems.js
@@ -6,7 +6,9 @@ export default function NasaItems(nasaItems) {
     <div class='cards__container'>
       ${nasaItems.collection.items
         .map((nasaItem) => {
-          if ((nasaItem.data[0].description.length >= 100) && (nasaItem.data[0].description.length <= 500)) {
+          if ((nasaItem.data[0].description.length >= 100) && 
+          (nasaItem.data[0].description.length <= 500) &&
+          (nasaItem.links[0].href.endsWith(".jpg"))) {
             return `
             ${NasaItem(nasaItem)}
               `;
