addab25f7242560f5bffab671dd2bc635500115a
KS <kitsidhu@gmail.com>
2021-07-22 15:56:33 -0400

    retrieve data from met api

--- client-side/js/components/Card.js
+++ client-side/js/components/Card.js
@@ -1,11 +1,11 @@
-export default function Card(card) {
-    return `
-    <div class='card__container'>
-        <input type='hidden' id='cardId${card.data[0].nasa_id}' value=${card.data[0].nasa_id} />
-        <h2 class='card__title'>${card.data[0].title}</h2>
-        <h3>${card.data[0].nasa_id}</h3>
-        <div class='card__img'><img src=${card.links[0].href} alt='' /></div>
-        <p class='card__desc'>Description: ${card.data[0].description}</p>
-    </div>
-    `;
-}
+// export default function Card(card) {
+//     return `
+//     <div class='card__container'>
+//         <input type='hidden' id='cardId${card.data[0].nasa_id}' value=${card.data[0].nasa_id} />
+//         <h2 class='card__title'>${card.data[0].title}</h2>
+//         <h3>${card.data[0].nasa_id}</h3>
+//         <div class='card__img'><img src=${card.links[0].href} alt='' /></div>
+//         <p class='card__desc'>Description: ${card.data[0].description}</p>
+//     </div>
+//     `;
+// }
---
+++ client-side/js/pages/MetObjectsPage.js
@@ -0,0 +1,10 @@
+import MetObjects from '../components/MetObjects.js';
+
+export default function MetObjectsPage() {
+    return `
+    <h1>MetObjects Page</h1>
+    <div class='cards__container'>
+
+    </div>
+    `;
+}
