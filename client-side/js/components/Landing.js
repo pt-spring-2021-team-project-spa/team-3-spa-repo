92c423e89b456bf1a27fe1110fe272fafa9f6448
Kouric-Calhoun <kouriccalhoun@gmail.com>
2021-07-23 20:53:53 -0400

    landing page

---
+++ client-side/js/components/Landing.js
@@ -0,0 +1,41 @@
+export default function Landing() {
+  return `
+    <ul class="nav-list">
+    <li><a href="" class="button">About Us</a></li>
+    <li><a href="" class="button">Contact Page</a></li>
+    <li><a href="" class="button">Mars Card List Page</a></li>
+    <li><a href="" class="button">Mars Page</a></li>
+    <li><a href="" class="button">Next</a></li>
+  </ul>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h1>Mystery Game</h1>
+      </div>
+    </div>
+
+    <audio controls autoplay loop>
+      <source
+        src="audio\Happy Background Music For Kids - Songs For Children.m4a"
+        type="audio/mpeg"
+      />
+    </audio>
+      `;
+}
+
+//     <nav role="navigation">
+//     <div class="menuToggle">
+//     <input type="checkbox" />
+//     <span></span>
+//     <span></span>
+//     <span></span>
+
+//     <ul class="menu">
+//       <a href="Home"><li>Home</li></a>
+//       <a href="Badges"><li>Badges</li></a>
+//       <a href="Resources"><li>Resources</li></a>
+//       <a href="About us"><li>About Us</li></a>
+//     </ul>
+//   </div>
+// </nav>
+// `;
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,25 +1,36 @@
-import Header from './components/Header.js';
-import apiActions from './api-actions/api-actions.js'
-import NasaItemsPage from './pages/NasaItemsPage.js';
-import NasaItemPage from './pages/NasaItemPage.js';
-import MetObjectsPage from './pages/MetObjectsPage.js';
-import MetObjectPage from './pages/MetObjectPage.js';
-import MetObject from './components/MetObject.js';
+import Header from "./components/Header.js";
+import apiActions from "./api-actions/api-actions.js";
+import NasaItemsPage from "./pages/NasaItemsPage.js";
+import NasaItemPage from "./pages/NasaItemPage.js";
+import MetObjectsPage from "./pages/MetObjectsPage.js";
+import MetObjectPage from "./pages/MetObjectPage.js";
+import MetObject from "./components/MetObject.js";
 // import Footer from "/components/Footer.js";
 // import HomePage from "./pages/HomePage.js";
-import RandomCard from './components/RandomCard';
-import ArtCard from './components/ArtCard.js';
-import PlanetCard from './components/PlanetCard.js';
+import RandomCard from "./components/RandomCard";
+import ArtCard from "./components/ArtCard.js";
+import PlanetCard from "./components/PlanetCard.js";
+import Landing from "./components/Landing.js";
 
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 const container = document.querySelector(".container");
 let nasaItemsJson;
 let metData = [];
-let nasaKeywords = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto'];
+let nasaKeywords = [
+  "mercury",
+  "venus",
+  "earth",
+  "mars",
+  "jupiter",
+  "saturn",
+  "uranus",
+  "neptune",
+  "pluto",
+];
 
 function randomCard() {
-  app.innerHTML = RandomCard(1)
-} 
+  app.innerHTML = RandomCard(1);
+}
 
 function header() {
   const headerElement = document.querySelector(".header");
@@ -40,13 +51,13 @@ function footer() {
 // }
 
 function renderNasaItemList() {
-  const nasaItemsButton = document.querySelector('.nav__list_nasa_cards');
-  nasaItemsButton.addEventListener('click', () => {
-    const app = document.querySelector('#app');
+  const nasaItemsButton = document.querySelector(".nav__list_nasa_cards");
+  nasaItemsButton.addEventListener("click", () => {
+    const app = document.querySelector("#app");
     let randIdx = Math.floor(Math.random() * nasaKeywords.length);
     apiActions.getRequest(
       // 'https://images-api.nasa.gov/search?keywords=mars',
-      'https://images-api.nasa.gov/search?keywords=' + nasaKeywords[randIdx],
+      "https://images-api.nasa.gov/search?keywords=" + nasaKeywords[randIdx],
       (nasaItems) => {
         nasaItemsJson = nasaItems;
         app.innerHTML = NasaItemsPage(nasaItems);
@@ -56,18 +67,17 @@ function renderNasaItemList() {
 }
 
 function renderNasaItem() {
-  const app = document.querySelector('#app');
-  app.addEventListener('click', (event) => {
-    if (event.target.classList.contains('card__title')) {
+  const app = document.querySelector("#app");
+  app.addEventListener("click", (event) => {
+    if (event.target.classList.contains("card__title")) {
       // console.log("NASA ID: " + event.target.parentNode.querySelector('#cardId').value)
-      const nasaId =
-        event.target.parentNode.querySelector('.id').value;
-        nasaItemsJson.collection.items.forEach((nasaItem, index) => {
-          let itemIndex = index;
-          if (nasaId === nasaItem.data[0].nasa_id) {
-            app.innerHTML = NasaItemPage(nasaItem, itemIndex);
-          }
-        })
+      const nasaId = event.target.parentNode.querySelector(".id").value;
+      nasaItemsJson.collection.items.forEach((nasaItem, index) => {
+        let itemIndex = index;
+        if (nasaId === nasaItem.data[0].nasa_id) {
+          app.innerHTML = NasaItemPage(nasaItem, itemIndex);
+        }
+      });
       //   const Url =
 
       // apiActions.getRequest(nasaItemUrl, (nasaItem) => {
@@ -93,82 +103,96 @@ function renderNasaItem() {
 //           // const card = document.querySelector(".card__inner");
 //           // card.addEventListener("click", function (e) {
 //           // card.classList.toggle('is-flipped');
-//           // });      
-//         }  
+//           // });
+//         }
 //         )
 //       }
-//     }  
-//   ) 
+//     }
+//   )
 // }
 
 function getRandomCard() {
-  const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+  const planets = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+  ];
   // fetch(location)
   //       .then((response) => response.json())
   //       .then((data) => callback(data))
   //       .then((err) => console.log(err));
-  fetch('http://localhost:8080/api/cards/random')
+  fetch("http://localhost:8080/api/cards/random")
     .then((response) => response.json())
-    .then(card => {
-      console.log(card.facts)
+    .then((card) => {
+      console.log(card.facts);
       if (planets.includes(card.name)) {
         // app.innerHTML = RandomCard(1);
-        console.log('planet card')
+        console.log("planet card");
         fetch(card.facts)
-        .then((response) => response.json())
-        .then(cardData => {
-          console.log(cardData)
-          app.innerHTML = PlanetCard(card.name, card.image, cardData)
-          setTimeout(function(){
-          const card = document.querySelector(".card__inner");
-          card.addEventListener("click", function (e) {
-          card.classList.toggle('is-flipped');
+          .then((response) => response.json())
+          .then((cardData) => {
+            console.log(cardData);
+            app.innerHTML = PlanetCard(card.name, card.image, cardData);
+            setTimeout(function () {
+              const card = document.querySelector(".card__inner");
+              card.addEventListener("click", function (e) {
+                card.classList.toggle("is-flipped");
+              });
+            }, 0);
           });
-        }, 0)
-
-        })  
       } else {
         fetch(card.facts)
-        .then((response) => response.json())
-        .then(cardData => {
-          console.log(cardData)
-          let x = app.innerHTML = ArtCard(card.name, card.image, cardData)
-          console.log("innerhtml done")
+          .then((response) => response.json())
+          .then((cardData) => {
+            console.log(cardData);
+            let x = (app.innerHTML = ArtCard(card.name, card.image, cardData));
+            console.log("innerhtml done");
 
-          setTimeout(function(){
-          const card = document.querySelector(".card__inner");
-          card.addEventListener("click", function (e) {
-          card.classList.toggle('is-flipped');
+            setTimeout(function () {
+              const card = document.querySelector(".card__inner");
+              card.addEventListener("click", function (e) {
+                card.classList.toggle("is-flipped");
+              });
+            }, 0);
           });
-        }, 0)
-
-        })  
       }
-    })
+    });
 }
 
 function renderMetObjectList() {
-    const metObjectsButton = document.querySelector('.nav__list_met_cards');
-    metObjectsButton.addEventListener('click', () => {
-      metData = [];
-      const app = document.querySelector('#app');
-      app.innerHTML = MetObjectsPage();
-      const metObjectsDiv = document.querySelector('.cards__container');
-      apiActions.getRequest(
-        'https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q=french',
-        (metObjectIds) => {
-          console.log(metObjectIds);
-          for (let i=0; i<40; i++) {
-            apiActions.getRequest(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${metObjectIds.objectIDs[Math.floor(Math.random() * metObjectIds.objectIDs.length)]}`, (metObject) => {
+  const metObjectsButton = document.querySelector(".nav__list_met_cards");
+  metObjectsButton.addEventListener("click", () => {
+    metData = [];
+    const app = document.querySelector("#app");
+    app.innerHTML = MetObjectsPage();
+    const metObjectsDiv = document.querySelector(".cards__container");
+    apiActions.getRequest(
+      "https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q=french",
+      (metObjectIds) => {
+        console.log(metObjectIds);
+        for (let i = 0; i < 40; i++) {
+          apiActions.getRequest(
+            `https://collectionapi.metmuseum.org/public/collection/v1/objects/${
+              metObjectIds.objectIDs[
+                Math.floor(Math.random() * metObjectIds.objectIDs.length)
+              ]
+            }`,
+            (metObject) => {
               console.log(MetObject);
               metData.push(metObject);
               metObjectsDiv.innerHTML += MetObject(metObject);
-            });
-          }
+            }
+          );
         }
-      );
-    });
-  }
+      }
+    );
+  });
+}
 
 // function renderMetCard() {
 //   console.log("getting to renderMetCard function");
@@ -207,7 +231,8 @@ function buildPage() {
   renderMetObjectList();
   // renderMetObject();
   // randomCard(); // will be taken out of buildpage after homepage is built
-  getRandomCard();
+  // getRandomCard();
+  Landing();
 }
 
-buildPage()
+buildPage();
